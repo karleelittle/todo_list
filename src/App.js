a1b4be0f8554c0736a08170e87fc8137651f79d1
Karlee Little <karleelittle1@gmail.com>
2022-07-23 02:21:04 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleNewTodoSubmit = (event) => {
+    event.preventDefault();
+
+    if (newTodo.length === 0){
+      return;
+    }
+
+    const todoItem = {
+      text: newTodo,
+      complete: false
+    }
+
+    setTodos([...todos, todoItem]);
+    setNewTodo("");
+  }
+  const handleTodoDelete = (deleteIndex) => {
+    const filteredTodos = todos.filter((_todo, i) => {
+      return i !== deleteIndex;
+    });
+
+    setTodos(filteredTodos);
+  }
+
+  const handleTodoComplete = (index) => {
+    const updatedTodos =todos.map((todo, i) => {
+      if (index === i) {
+        todo.complete = !todo.complete;
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign: "center"}}>
+      <form onSubmit={(event) => {
+      handleNewTodoSubmit(event);
+      }}>
+        <input onChange={(event) => {
+          setNewTodo(event.target.value);
+        }} type="text"
+        value={newTodo} />
+        <div>
+          <button>Add New Todo</button>
+        </div>
+      </form>
+      <hr />
+      {todos.map((todo, i) => {
+
+        const todoClasses = [];
+        if (todo.complete) {
+          todoClasses.push("line-through");
+        }
+
+          return ( 
+          <div key={i}>
+            <span className={todoClasses.join(" ")}>{todo.text} </span>
+            <button onClick={(event) => {
+            handleTodoDelete(i);
+            }}>Delete</button>
+            <input onChange={(event) => {
+              handleTodoComplete(i);
+            }} checked={todo.complete} type="checkbox" />
+          </div>
+          );
+      })}
+
     </div>
   );
 }
